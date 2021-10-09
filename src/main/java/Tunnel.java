7db1c4597d66a4f073b2da4d90fb35bb8b535c3d
kikimarik <kikimarik@inbox.ru>
2021-10-09 20:57:17 +0300

    synchronize the Race

---
+++ src/main/java/Tunnel.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.Semaphore;
+
+public class Tunnel extends Stage {
+
+    private final Semaphore semaphore;
+
+    public Tunnel(Semaphore semaphore) {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                semaphore.acquire();
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                semaphore.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
